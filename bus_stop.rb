ab9ff2ff44e8dfeeacffab50bd4d36b534d08217
David Eastcroft <davideastcroft95@gmail.com>
2020-03-18 20:58:21 +0000

    Adds test and function for adding people to queue.

--- bus_stop.rb
+++ bus_stop.rb
@@ -7,4 +7,14 @@ class BusStop
     @queue = []
   end
 
+  def queue_length
+    return @queue.length
+  end
+
+  def add_person_to_queue(people)
+    for person in people
+      @queue << person
+    end
+  end
+
 end
--- specs/bus_stop_spec.rb
+++ specs/bus_stop_spec.rb
@@ -3,15 +3,29 @@ require('minitest/reporters')
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 require_relative('../bus_stop.rb')
+require_relative('../person.rb')
+
 
 class TestBusStop < Minitest::Test
 
   def setup
-    @bus_stop = BusStop.new("Balamb Town")
+    @bus_stop = BusStop.new("Glasgow Quay")
+    @person_1 = Person.new("Amy", 23)
+    @person_2 = Person.new("David", 25)
+    @people = [@person_1, @person_2]
   end
 
   def test_get_name
-    assert_equal("Balamb Town", @bus_stop.name)
+    assert_equal("Glasgow Quay", @bus_stop.name)
+  end
+
+  def test_queue_length
+    assert_equal(0, @bus_stop.queue_length)
+  end
+
+  def test_add_person_to_queue
+    @bus_stop.add_person_to_queue(@people)
+    assert_equal(2, @bus_stop.queue_length)
   end
 
 end
