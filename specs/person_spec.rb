134f2fc526f622e809fd0904f015cf0989a8f7b4
David Eastcroft <davideastcroft95@gmail.com>
2020-03-18 22:04:41 +0000

    Changes comparison name.

--- specs/person_spec.rb
+++ specs/person_spec.rb
@@ -7,15 +7,15 @@ require_relative('../person.rb')
 class PersonTest < MiniTest::Test
 
   def setup
-    @person = Person.new("Jack", 22)
+    @person = Person.new("Amy", 23)
   end
 
   def test_get_name
-    assert_equal("Jack", @person.name)
+    assert_equal("Amy", @person.name)
   end
 
   def test_get_age
-    assert_equal(22, @person.age)
+    assert_equal(23, @person.age)
   end
 
 end
